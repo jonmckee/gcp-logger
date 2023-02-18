eee7a6ef01910a10f76b350e645000227fcf4e74
Jon McKee <jmckee@theknotww.com>
2023-02-18 16:31:32 -0600

    Fix export

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-const correlator = require('correlation-id')
-const winston = require('winston')
+import correlator from "correlation-id";
+import winston from "winston";
 
-module.exports = {
-    withCorrelationId: (handler) => (req, res) => {
-        const existingId = req.headers['correlation-id']
+const withCorrelationId = (handler) => (req, res) => {
+    const existingId = req.headers['correlation-id']
 
-        if (existingId) {
-            correlator.withId(existingId, () => handler(req, res))
-        } else {
-            correlator.withId(() => {
-                req.headers['correlation-id'] = correlator.getId()
+    if (existingId) {
+        correlator.withId(existingId, () => handler(req, res))
+    } else {
+        correlator.withId(() => {
+            req.headers['correlation-id'] = correlator.getId()
 
-                handler(req, res)
-            })
-        }
-    },
-    createLogger: () => winston.createLogger({
-        format: winston.format.combine(
-            winston.format((info) => {
-                info.correlationId = correlator.getId()
-
-                return info
-            })(),
-            winston.format.json()
-        ),
-        transports: [new winston.transports.Console()],
-    })
+            handler(req, res)
+        })
+    }
 }
 
+const createLogger = () => winston.createLogger({
+    format: winston.format.combine(
+        winston.format((info) => {
+            info.correlationId = correlator.getId()
+
+            return info
+        })(),
+        winston.format.json()
+    ),
+    transports: [new winston.transports.Console()],
+})
+
+export {
+    createLogger,
+    withCorrelationId
+}
